767fffdd43758dec4c6876185762d163203d000a
ngall <nel.gall@gmail.com>
2014-05-27 02:21:03 +0200

    Creating Earth

--- js/app.js
+++ js/app.js
@@ -1,48 +1,106 @@
 var container = document.getElementById('container');
-var renderer = new THREE.CanvasRenderer();
+var renderer = new THREE.WebGLRenderer( { alpha: true } );
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 1, 10000);
-var distance = 1000;
-var nbObjects = 1000;
-var geometry = new THREE.Geometry();
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 1, 10000);
+
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 container.appendChild(renderer.domElement);
 
-for (var i = 0; i < nbObjects; i++) {
-	var particle = new THREE.Particle(new THREE.ParticleCanvasMaterial({
-		color: Math.random() * 0x808080 + 0x808080, 
-		opacity: 0.5,
-		program: function (context) {
-			context.beginPath();
-			context.arc(0,0,1,0,Math.PI * 2, true);
-			context.closePath();
-			context.fill();
-		}
-	}));
 
-	particle.position.x = Math.random() * distance * 4 - distance * 2;
-	particle.position.y = Math.random() * distance * 4 - distance * 2;
-	particle.position.z = Math.random() * distance * 4 - distance * 2;
-	particle.scale.x = particle.scale.y = Math.random() * 10 + 5;
+/**
+ *  EARTH
+ */
+var geometry  = new THREE.SphereGeometry(1, 64, 64);
 
-	scene.add(particle);
-}
+var material         = new THREE.MeshPhongMaterial();
+material.map         = THREE.ImageUtils.loadTexture('img/earthmap1k.jpg');
+material.bumpMap     = THREE.ImageUtils.loadTexture('img/earthbump1k.jpg');
+material.specularMap = THREE.ImageUtils.loadTexture('img/earthspec1k.jpg');
+material.specular    = new THREE.Color('#555555');
+material.bumpScale   = 0.05;
 
-camera.position.z = 100;
-camera.lookAt(scene.position);
+var earthMesh = new THREE.Mesh(geometry, material);
+
+earthMesh.position.x = 0;
+earthMesh.position.y = 0;
+earthMesh.position.z = 0;
+
+
+/**
+ * CLOUDS
+ */
+var cloudMesh = new THREE.Mesh(
+  new THREE.SphereGeometry(1.01, 64, 64),
+  new THREE.MeshPhongMaterial({
+    map: THREE.ImageUtils.loadTexture('img/fair_clouds_4k.png'),
+    transparent: true
+  })
+);
+earthMesh.add(cloudMesh);
+
+
+/**
+ * LIGHTS
+ */
+var light = new THREE.DirectionalLight( 0xffffee, 0.8 );
+light.position.set(2,2,10);
+
+
+/**
+ * UNIVERSE
+ */
+var geometry  = new THREE.SphereGeometry(12, 16, 16);
+var material  = new THREE.MeshBasicMaterial();
+material.map  = THREE.ImageUtils.loadTexture('img/galaxy_starfield.png');
+material.side = THREE.BackSide;
+var spaceMesh = new THREE.Mesh(geometry, material);
+
+
+/**
+ * MANAGE CAMERA
+ */
+camera.position.z = 3;
+camera.lookAt(earthMesh.position);
+
+
+/**
+ * ADDING TO THE SCENE
+ */
+scene.add(light);
 scene.add(camera);
+scene.add(earthMesh);
+scene.add(spaceMesh);
 
+
+/**
+ * RENDERING
+ */
 renderer.render(scene, camera);
 
-document.addEventListener('mousemove', onMouseMove, false);
 
-function onMouseMove(event) {
-	var mouseX = event.clientX - window.innerWidth/2;
-	var mouseY = event.clientY - window.innerHeight/2;
-	camera.position.x += (mouseX - camera.position.x) * 0.05;
-	camera.position.y += (mouseY - camera.position.y) * 0.05;
-	camera.position.z = distance;
-	camera.lookAt(scene.position);
+/**
+ * MOUSE LISTENER
+ */
+var mouse = {x : 0, y : 0};
+document.addEventListener('mousemove', function(event){
+	mouse.x = (event.clientX / window.innerWidth ) - 0.5;
+	mouse.y = (event.clientY / window.innerHeight) - 0.5;
+	camera.position.x += (mouse.x*10 - camera.position.x) / 1;
+	camera.position.y += (mouse.y*10 - camera.position.y) / 1;
+	camera.lookAt( scene.position );
 	renderer.render(scene, camera);
-}
+}, false);
+
+
+/**
+ *  ANIMATION
+ */
+function animate(){
+    requestAnimationFrame(animate);
+    earthMesh.rotation.y += 1/256 ;
+    cloudMesh.rotation.y += 1/1200 ;
+    renderer.render(scene, camera);
+};
+
+animate();
