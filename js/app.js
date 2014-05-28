ececdce42abace6684fa96b5839e784360b17f95
Nelson <nel.gall@gmail.com>
2014-05-29 01:52:22 +0200

    better calculations

--- js/app.js
+++ js/app.js
@@ -6,10 +6,17 @@ var renderer = new THREE.WebGLRenderer( { alpha: true, antialias: false } );
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(40, window.innerWidth/window.innerHeight, 0.1, 10000);
 var controls, stats;
+
+var nbTours, inc = 0;
 var target = "earth";
 
+
+var earthRadius = 1.0;
 var earthDistance = 20;
-var earthRadius = 1;
+var earthDaySpeed = Math.PI/240; 
+var earthYearSpeed = earthDaySpeed/365.25; 
+
+var sunRadius = 2.0;
 
 
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -26,7 +33,7 @@ var material         = new THREE.MeshPhongMaterial();
 material.map         = THREE.ImageUtils.loadTexture('img/earth/earthmap2k.jpg');
 material.bumpMap     = THREE.ImageUtils.loadTexture('img/earth/earthbump2k.jpg');
 material.specularMap = THREE.ImageUtils.loadTexture('img/earth/earthspec2k.jpg');
-material.specular    = new THREE.Color('#444444');
+material.specular    = new THREE.Color('#151515');
 material.side        = THREE.DoubleSide;
 material.bumpScale   = 0.06;
 
@@ -52,7 +59,7 @@ for (var i = 0; i <= segmentCount; i++) {
 }
 
 var equator = new THREE.Line(geometry, material);
-earth.rotation.x = Math.PI/10
+earth.rotation.x = (Math.PI/180)*23.5;
 earth.add( equator );
 
 
@@ -126,7 +133,7 @@ material = new THREE.ShaderMaterial( {
 
 } );
 
-var sun = new THREE.Mesh(new THREE.SphereGeometry(2, 32, 32), material);
+var sun = new THREE.Mesh(new THREE.SphereGeometry(sunRadius, 32, 32), material);
 
 var customMaterialAtmosphere = new THREE.ShaderMaterial({
 	uniforms: {
@@ -144,7 +151,7 @@ scene.add(sun);
 /**
  * LIGHTS
  */
-var light = new THREE.PointLight( 0xffffee, 0.7 );
+var light = new THREE.PointLight( 0xffffee, 1, 0);
 light.position.set(0,0,0);
 scene.add(light);
 scene.add(new THREE.AmbientLight(0x111111));
@@ -165,9 +172,9 @@ scene.add(space);
 /**
  * CAMERA
  */
-camera.position.x = 5;
-camera.position.y = 5;
-camera.position.z = 10;
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 40;
 camera.lookAt(earth.position);
 scene.add(camera);
 
@@ -204,7 +211,8 @@ controls.addEventListener( 'change', render );
 controls.center.set(earth.position.x, earth.position.y, earth.position.z);
 
 
-var theta = 0.001;
+var theta = 0; 
+
 animate();
 
 
@@ -236,16 +244,29 @@ function render() {
  * ANIMATION
  */
 function animate() {
+	
+	nbTours = Math.floor(earth.rotation.y / (Math.PI*2));
+
+	if (inc == nbTours) {
+		console.log(nbTours + ' jour(s)');
+		inc++;
+	}
+
 
     requestAnimationFrame(animate);
 
 	uniforms.time.value -= 0.015;
 
-	earth.rotation.y += 0.01 ;
-	clouds.rotation.y -= 0.0005 ;
+	earth.rotation.y += earthDaySpeed;
+	//clouds.rotation.y -= earthDaySpeed-0.02 ;
+
 	
-	earth.position.x = earth.position.x * Math.cos(theta) + earth.position.z * Math.sin(theta);
-	earth.position.z = earth.position.z * Math.cos(theta) - earth.position.x * Math.sin(theta);
+	//earth.position.x = earth.position.x * Math.cos(theta) + earth.position.z * Math.sin(theta);
+	//earth.position.z = earth.position.z * Math.cos(theta) - earth.position.x * Math.sin(theta);
+
+	theta += earthYearSpeed;
+	earth.position.x = earthDistance * Math.sin(theta);
+	earth.position.z = earthDistance * Math.cos(theta);
 	
 	if (target == "earth")
 		controls.center.set(earth.position.x, earth.position.y, earth.position.z);
